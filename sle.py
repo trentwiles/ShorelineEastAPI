69213ad302e0f389a93fdd8b7c6a5aa44b55d3fe
trentwiles <wilest7@gmail.com>
2023-10-25 14:18:06 -0700

    basic product

--- sle.py
+++ sle.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-def stationNameToStationNumber(number):
+def stationNameToStationNumber(name):
     dictionary = {
         "New London": 1,
         "Old Saybrook": 2,
@@ -13,18 +13,50 @@ def stationNameToStationNumber(number):
         "New Haven State Street Station": 8,
         "New Haven Union Station": 9
     }
-    return
+    return dictionary[name]
+
 def getTrainHTML(startStation, endStation, travelDate, travelTime):
-    r = requests.post("https://shorelineeast.com/schedules/trip-planner", data={"fromStation": stationNameToStationNumber(startStation), "toStation": stationNameToStationNumber(endStation), "travel_date": travelDate, "way": 2, "time": travelTime, "return": "false"})
-    print(r.status_code)    
-    soup = BeautifulSoup(r.text, "html.parser")
-    table = soup.find("th")
 
-    print(table)
+    url = "https://shorelineeast.com/schedules/trip-planner"
+    headers = {}
 
-    for x in table.find("thead"):
-        print(x)
-        print("^^^^^^^^^^^^^^^^^^^^")
+    data = {
+        "fromstation": stationNameToStationNumber(startStation),
+        "tostation": stationNameToStationNumber(endStation),
+        "travel_date": travelDate,
+        "way": "2",
+        "time": travelTime,
+        "return": "false",
+    }
 
+    response = requests.post(url, data=data)
 
-getTrainHTML("Madison", "Guilford", "10/25/2023", "7:00+AM")
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find("table")
+    trains = table.find_all("td")
+
+    departTimes = []
+    trainIDs = []
+
+    travelTime = []
+    arrivalTimes = []
+
+    counter = 0
+    for train in trains:
+        if (counter % 3) == 0: # only get every third item, those have the valuable times
+            departTimes.append(train.get_text(strip=True)[:-4])
+            trainIDs.append(train.find("span").text.strip())
+        elif "AM" in train.text or "PM" in train.text:
+            arrivalTimes.append(train.text.strip())
+        else:
+            travelTime.append(train.text.strip())
+        counter += 1
+    
+    final = []
+    for x in range(len(travelTime)):
+        final.append({"trainNumber": trainIDs[x], "trainTime": departTimes[x], "timeTimeArrive": arrivalTimes[x], "trainTravelTime": travelTime[x]})
+    
+    return final
+    
+
+print(getTrainHTML("Madison", "Guilford", "10/30/2023", "9:00 AM"))
