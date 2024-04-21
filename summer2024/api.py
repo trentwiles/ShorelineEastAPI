97366f7288aefb70bfa85f568fdf4844df544996
trentwiles <wilest7@gmail.com>
2024-04-21 01:41:10 +0100

    work and progress

--- summer2024/api.py
+++ summer2024/api.py
@@ -1,11 +1,122 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import time
+import sys
 
-def translateStationToId(name:str):
-    return
+WESTERN_TERMINUS = "New Haven Union Station"
+WESTERN_TERMINUS_ID = "9"
+EASTERN_TERMINUS = "New London"
+EASTERN_TERMINUS_ID = "1"
 
-def translateIDtoStation(id:int):
-    return
+USER_AGENT = "Shore Line East API (+https://github.com/trentwiles/ShorelineEastAPI)"
 
-def getAllTrainsAtStation():
-    return
+def formatDate(date):
+    # Shoreline East wants dates to look like this:
+    # 04/01/2024
+    # 
+    # Which is a bit of a problem because Python prints
+    # out dates like this:
+    # 4/1/2024
+    #
+    # This code will fix that
+
+    if int(date) >= 10:
+        return date
+    
+    return f"0{date}"
+
+def formatTime(tme):
+    if int(tme) > 12:
+        return str(int(tme) - 12)
+    return tme
+
+def translateStationToId(name):
+    stations = {
+        "New London": "1",
+        "Old Saybrook": "2",
+        "Westbrook": "3",
+        "Clinton": "4",
+        "Madison": "5",
+        "Guilford": "6",
+        "Branford": "7",
+        "New Haven State Street": "8",
+        "New Haven Union Station": "9"
+    }
+
+    if name in stations:
+        return stations[name]
+    else:
+        return None
+
+def translateIDtoStation(id):
+    stations = {
+        "1": "New London",
+        "2": "Old Saybrook",
+        "3": "Westbrook",
+        "4": "Clinton",
+        "5": "Madison",
+        "6": "Guilford",
+        "7": "Branford",
+        "8": "New Haven State Street",
+        "9": "New Haven Union Station"
+    }
+    if id in stations:
+        return stations[id]
+    else:
+        return None
+    
+def getCallingAt(direction):
+    if direction.lower() == "westbound":
+        return
+        
+
+def getAllTrainsAtStation(time:int, stationName:str):
+    # get the station ID
+    stationID = translateStationToId(stationName)
+    if stationID == None:
+        return None
+    
+    # convert timestamp
+    dt = datetime.datetime.fromtimestamp(time)
+    year = str(dt.year)
+    month = str(dt.month)
+    day = str(dt.day)
+    hour = str(dt.hour)
+    amPM = 'AM' if int(hour) < 12 else 'PM'
+    minute = str(dt.minute)
+
+    # IMPORTANT: NEED METHODS TO DETERMINE IF THE STATION INPUTTED IS THE TERMINUS
+
+    # eastbound
+    """
+    fromstation	"1"
+tostation	"9"
+travel_date	"04/22/2024"
+way	"2"
+time	"7:00+AM"
+return	"false"
+
+    """
+    dataTravelDate = f"{formatDate(month)}/{formatDate(day)}/{year}"
+    dataTime = f"{formatTime(hour)}:{formatDate(minute)}+{amPM}"
+
+    data = {
+        "fromstation": stationID,
+        "tostation": WESTERN_TERMINUS_ID,
+        "travel_date": dataTravelDate,
+        "way": "2", # no idea what this is
+        "time": dataTime,
+        "return": "false" # one way only
+    }
+    r = requests.post("https://shorelineeast.com/schedules/trip-planner", data=data, headers={"User-agent": USER_AGENT})
+    se = BeautifulSoup(r.text, "html.parser")
+    times = []
+    for train in se.find_all("div", {"class": "result_box long"}):
+        if train != None and "Depart" in train.text:
+            print(train.text)
+        print("--------------")
+
+    print(r.status_code)
+
+print(getAllTrainsAtStation(round(time.time()),"Branford"))
