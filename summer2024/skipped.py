c641e6b14f51128d698d8fd547f314be093bd9d8
trentwiles <wilest7@gmail.com>
2024-04-29 08:21:29 -0400

    skipped stations

---
+++ summer2024/skipped.py
@@ -0,0 +1,34 @@
+
+# sourced from https://shorelineeast.com/wp-content/uploads/2024/04/SLE_Sched_Apr7_8R2.pdf
+# valid as off
+
+def getStationsSkipped(trainNumber):
+    if trainNumber == "3690":
+        return ["New Haven State Street"]
+    
+    if trainNumber == "3692":
+        return ["Madison"]
+    
+    if trainNumber == "3604":
+        return ["Madison"]
+    
+    if trainNumber == "3610":
+        return ["Madison"]
+    
+    if trainNumber == "1610":
+        return ["Madison"]
+    
+    if trainNumber == "1667":
+        return ["Madison"]
+    
+    if trainNumber == "1681":
+        return ["Madison"]
+    
+    if trainNumber == "1687":
+        return ["Madison"]
+    
+    if trainNumber == "1691":
+        return ["Madison"]
+    
+    if trainNumber == "1699":
+        return ["Madison"]
