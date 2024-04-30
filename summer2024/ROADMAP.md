63d7befd48d4ef1007e0a7e7a3a7609d610e79ab
trentwiles <wilest7@gmail.com>
2024-04-30 08:15:33 -0400

    basic test

--- summer2024/ROADMAP.md
+++ summer2024/ROADMAP.md
@@ -1,11 +1,16 @@
 # Project Roadmap
 
 ## Uncompleted
-* Web API
-* Advanced calling list
+* Code commenting
 * "Change here for amtrak/CT Rail" announcement, "This service will not stop at X. For x, change, and wait here for the next train."
+* API Documentation
+* Fare web API
 
+## In Progress
+* Advanced calling list
 
 ## Completed
+* Web API
 * Basic train time scraper, with times and origin
 * Basic list of stations train will stop (UK: "call") at
+* Fare calculator
--- summer2024/tests.py
+++ summer2024/tests.py
@@ -1,5 +1,14 @@
 import api, time, json
+import unittest
 
-#print(api.getCallingAt("eastbound", "New Haven Union Station"))
-print(json.dumps(api.getAllTrainsAtStation(round(time.time()), "Madison", True)))
+class TestCuboid(unittest.TestCase):
+    def test_calling(self):
+        expected = {'currentStation': 'New Haven Union Station',
+                    'direction': 'eastbound',
+                    'callingAt': ['New Haven State Street', 'Branford', 'Guilford', 'Madison', 'Clinton', 'Westbrook', 'Old Saybrook', 'New London'],
+                     'terminatesAt': 'New London'}
+        self.assertAlmostEqual(api.getCallingAt("eastbound", "New Haven Union Station"), expected)
+
+print()
+#print(json.dumps(api.getAllTrainsAtStation(round(time.time()), "Madison", True)))
 #print(api.parseTime("7:15 AM", 21, 4, 2024))
