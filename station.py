eca872ae85d0a196d175e75b9843cca5a8b52b79
trentwiles <wilest7@gmail.com>
2024-08-26 14:09:59 -0400

    scope out HTML

--- station.py
+++ station.py
@@ -15,6 +15,29 @@ def getPageDetails(station):
         'Old Saybrook': 'old-saybrook-station',
         'New London': 'new-london-station'
     }
-    r = requests.get()
+    
+    try:
+        formatStation = stationToStationPage[station]
+    except KeyError:
+        return {"error": True, "message": "Invalid station name"}
+    
+    r = requests.get(f"https://shorelineeast.com/stations/{formatStation}/")
+    soup = BeautifulSoup(r.text, "html.parser")
+    counter = 1
+    
+    # Guide to WPB wraper
+    # 1 & 2 - Name & Title (https://trentwil.es/a/eYhC8LxyIG.png)
+    # 3 - Photo (https://trentwil.es/a/uDYgWjIqWg.png)
+    # 4 & 5 - Address + Ticket Sales + Accessability (https://trentwil.es/a/Ou7cNK4xw9.png)
+    # 6 & 7 - Trip Planning Button (https://trentwil.es/a/UKtxZEnbdo.png)
+    # 8 & 9 - Elevator status, station information, bus substitution, parking, directions (https://trentwil.es/a/VJtQMsJbUw.png)
+    # 10 - Directions
+    # 11 & 12 - Trip planning
+    
+    for x in soup.find_all("div", {"class": "wpb_wrapper"}):
+        print(f"{str(counter)}. ================================================")
+        print(x)
+        counter += 1
+        
 
     return
---
+++ tester.py
@@ -0,0 +1,3 @@
+import station
+
+station.getPageDetails('Westbrook')
